707ad79d224a8939a0b2f8ee6cd9a0e885c7a5d1
ahmedafraz007 <a_ahmedafrazbacker@fanshaweonline.ca>
2024-07-31 19:51:36 -0400

    afs

--- week13-LA2-S2/scripts/app.js
+++ week13-LA2-S2/scripts/app.js
@@ -6,15 +6,11 @@ const app = Vue.createApp({
             studentName: `afrz`,
             pressure: 50,
             pressureLevels: [
-                 { id: 'danger-low', color: 'danger', label: 'Danger - Low!', min: 0, max:
-                10 },
-                 { id: 'warn-low', color: 'warning', label: 'Warning - Low', min: 11, max:
-                30 },
+                 { id: 'danger-low', color: 'danger', label: 'Danger - Low!', min: 0, max: 10 },
+                 { id: 'warn-low', color: 'warning', label: 'Warning - Low', min: 11, max:30 },
                 { id: 'safe', color: 'safe', label: 'Safe', min: 31, max: 70 },
-                { id: 'warn-high', color: 'warning', label: 'Warning - High', min: 71,
-                max: 90 },
-                { id: 'danger-high', color: 'danger', label: 'Danger - High', min: 91,
-                max: 100 }
+                { id: 'warn-high', color: 'warning', label: 'Warning - High', min: 71, max: 90 },
+                { id: 'danger-high', color: 'danger', label: 'Danger - High', min: 91, max: 100 }
                  ]
         };
     },
